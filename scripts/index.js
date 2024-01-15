8f2d80a219e66980aa5d1e2a9c12c5084e841320
George Mikheev <GeorgeMikheev56@yandex.ru>
2024-01-15 13:35:55 +0300

    added function delete cards

--- scripts/index.js
+++ scripts/index.js
@@ -9,16 +9,19 @@
 // @todo: Вывести карточки на страницу
 
 //Переменные:
-
 const places = document.querySelector('.places'); // Секция с карточками
 const cardList = places.querySelector('.places__list'); // Список карточек
 const cardTemplet = document.querySelector('#card-template').content; // Выбрал template карточки со свойством content 
+    
 
 
-//Функция создания карточки:
+
+//Функция создания и удаления карточки:
 
 function addCards(card) {
     const cardElement = cardTemplet.cloneNode(true); // Клонирую содержимое карточки
+    const deletButton = cardElement.querySelector('.card__delete-button');
+    
 
     // Добавляю данные из cards.js:
     cardElement.querySelector('.card__image').src = card.link;
@@ -27,18 +30,15 @@ function addCards(card) {
 
     // Добавляю карточки на страницу:
     cardList.append(cardElement);
+
+
+    // Удаление карточки:
+    
+    deletButton.addEventListener('click', function () {
+        deletButton.closest('.card').remove()     
+    });
 };
 
 //Вывод карточек:
 
 initialCards.forEach((card) => addCards(card));
-
-//Удаление карточки:
-
-const deletButton = cardTemplet.querySelector('card__delete-button');
-
-deletButton.addEventListener('click', function () {
-    alert('check');
-});
-
-
