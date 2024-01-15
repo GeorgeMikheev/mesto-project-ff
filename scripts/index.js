95b37cc08c6e67dbfc5d23a1388a1e545e0a13de
George Mikheev <GeorgeMikheev56@yandex.ru>
2024-01-15 20:31:27 +0300

    added new function

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,27 @@
-// @todo: Темплейт карточки
+// Переменные:
+const cardsContainer = document.querySelector('.places__list'); // Список карточек
+const cardTemplete = document.querySelector('#card-template').content; // Выбрал template карточки со свойством content 
 
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
-
-//Переменные:
-const places = document.querySelector('.places'); // Секция с карточками
-const cardList = places.querySelector('.places__list'); // Список карточек
-const cardTemplet = document.querySelector('#card-template').content; // Выбрал template карточки со свойством content 
+// Функция создания карточек:
+function createCard(card, deleteCardEvent) {
+    const cardElement = cardTemplete.cloneNode(true); // Клонирую содержимое карточки
+    const deleteButton = cardElement.querySelector('.card__delete-button'); // Кнопка удаления
+    const cardImage = cardElement.querySelector('.card__image'); // Картинки внутри карточек
     
-
-
-
-//Функция создания и удаления карточки:
-
-function addCards(card) {
-    const cardElement = cardTemplet.cloneNode(true); // Клонирую содержимое карточки
-    const deletButton = cardElement.querySelector('.card__delete-button');
-    
-
-    // Добавляю данные из cards.js:
-    cardElement.querySelector('.card__image').src = card.link;
-    cardElement.querySelector('.card__image').alt = card.name;
+    // Добавляю данные из card.js:
+    cardImage.src = card.link;
+    cardImage.alt = card.name;
     cardElement.querySelector('.card__title').textContent = card.name;
-
-    // Добавляю карточки на страницу:
-    cardList.append(cardElement);
-
-
-    // Удаление карточки:
     
-    deletButton.addEventListener('click', function () {
-        deletButton.closest('.card').remove()     
-    });
-};
+    deleteButton.addEventListener('click', deleteCardEvent); // Event вызывающий функцию удаления карточки
 
-//Вывод карточек:
+    return cardElement; // Возвращаю готовую карточку
+}
 
-initialCards.forEach((card) => addCards(card));
+// Функция удаления карточки:
+function deleteCard() {
+    this.closest('.card').remove();
+}
+
+// Добавление карточки в разметку:
+initialCards.forEach((card) => cardsContainer.append(createCard(card, deleteCard)));
